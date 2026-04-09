bdb1b6a977a4bf1c11ce3e811cd2cd08309f5dbe
MrJetby <aminfazlonshoev@gmail.com>
2026-04-09 16:46:14 -0400

    Change `Audience.audience(player).sendActionBar(context)` to `player.sendActionBar(context)`

--- src/main/java/me/jetby/libb/action/impl/ActionBarImpl.java
+++ src/main/java/me/jetby/libb/action/impl/ActionBarImpl.java
@@ -3,7 +3,6 @@ package me.jetby.libb.action.impl;
 import me.jetby.libb.Libb;
 import me.jetby.libb.action.Action;
 import me.jetby.libb.action.ActionContext;
-import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -19,7 +18,7 @@ public class ActionBarImpl implements Action {
 
         if (line != null) {
             Component context = Libb.MINI_MESSAGE.deserialize(line);
-            Audience.audience(player).sendActionBar(context);
+            player.sendActionBar(context);
         }
     }
 }
